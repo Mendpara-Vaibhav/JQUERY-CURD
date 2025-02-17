ca227ae31f9c035c238027deff37070209632afe
Mendpara Vaibhav <mendparavaibhav@gmail.com>
2025-02-17 19:54:38 +0530

    completed

--- script.js
+++ script.js
@@ -7,9 +7,32 @@ $(document).ready(function () {
     let formData = readFormData();
     console.log(formData);
 
-    arrList.push(formData);
+    if (selectedRowIndex === null) {
+      arrList.push(formData);
+    } else {
+      arrList[selectedRowIndex] = formData;
+      selectedRowIndex = null;
+    }
     updateTable();
     resetForm();
+  });
+
+  $(document).on("click", ".onEdit", function () {
+    selectedRowIndex = $(this).data("index");
+    let selectedRow = arrList[selectedRowIndex];
+
+    $("#fname").val(selectedRow.fname);
+    $("#lname").val(selectedRow.lname);
+  });
+
+  $(document).on("click", ".onDelete", function () {
+    let index = $(this).data("index");
+    console.log(index);
+    if (confirm("Are you sure you want to delete")) {
+      arrList.splice(index, 1);
+      updateTable();
+      resetForm();
+    }
   });
 });
 
@@ -28,12 +51,12 @@ function updateTable() {
     tableData += `<tr>`;
     tableData += `<td>${element.fname}</td>`;
     tableData += `<td>${element.lname}</td>`;
-    tableData += `<td><a onclick=onEdit(${index})>Edit</a> /
-                      <a onclick=onDelete(${index})>Delete</a></td>`;
+    tableData += `<td><a class="onEdit" data-index="${index}">Edit</a> /
+                      <a class="onDelete" data-index="${index}">Delete</a></td>`;
     tableData += `</tr>`;
   });
   $("#tableBody").html(tableData);
-  console.log(arrList);
+  // console.log(arrList);
 }
 
 function resetForm() {
